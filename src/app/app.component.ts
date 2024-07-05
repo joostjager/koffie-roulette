99f447f51aa3395abc789209a7a0fb984dc888e3
Joost Jager <joost.jager@gmail.com>
2024-07-05 19:16:42 +0200

    cups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoffeeCupDisplayComponent } from './coffee-cup-display/coffee-cup-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoffeeCupDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'koffie-roulette';
+
+  coffeeCount: number = 0;
+
+  increaseCount() {
+    this.coffeeCount++;
+  }
+
+  decreaseCount() {
+    if (this.coffeeCount > 0) {
+      this.coffeeCount--;
+    }
+  }
 }
+
---
+++ src/app/coffee-cup-display/coffee-cup-display.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-coffee-cup-display',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './coffee-cup-display.component.html',
+  styleUrl: './coffee-cup-display.component.css'
+})
+export class CoffeeCupDisplayComponent {
+  @Input() count: number = 0;
+
+  get cups() {
+    return new Array(this.count);
+  }
+}
