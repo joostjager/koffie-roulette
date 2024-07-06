b4ea6ae7d7e4fa164e90e3388ebc0f344530af66
Joost Jager <joost.jager@gmail.com>
2024-07-06 10:07:19 +0200

    two cols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,30 @@ export class AppComponent {
 
   capuCount: number = 0;
   coffCount: number = 0;
+
+  capuChance: number = 0;
+  coffChance: number = 0;
+
+  capuPrice: number = 1.9;
+  coffPrice: number = 1.25;
+
+  onCapuCountChanged(newValue: number) {
+    this.capuCount = newValue;
+
+    this.update();
+  }
+
+  onCoffCountChanged(newValue: number) {
+    this.coffCount = newValue;
+
+    this.update();
+  }
+
+  update(): void {
+    var totalPrice = this.capuCount * this.capuPrice + this.coffCount * this.coffPrice;
+
+    this.capuChance = this.capuPrice / totalPrice;
+    this.coffChance = this.coffPrice / totalPrice;
+  }
 }
 
--- src/app/coffee-cup-display/coffee-cup-display.component.ts
+++ src/app/coffee-cup-display/coffee-cup-display.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgFor, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coffee-cup-display',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, CommonModule],
   templateUrl: './coffee-cup-display.component.html',
   styleUrl: './coffee-cup-display.component.css'
 })
 export class CoffeeCupDisplayComponent {
   @Input() count: number = 0;
+  @Input() chance: number = 0;
+
+  @Output() countChanged: EventEmitter<number> = new EventEmitter<number>();
 
   get cups() {
     return new Array(this.count);
@@ -20,11 +23,16 @@ export class CoffeeCupDisplayComponent {
       return;
 
     this.count++;
+
+    this.countChanged.emit(this.count);
   }
 
   decreaseCount() {
-    if (this.count > 0) {
-      this.count--;
-    }
+    if (this.count == 0)
+      return;
+
+    this.count--;
+
+    this.countChanged.emit(this.count);
   }
 }
