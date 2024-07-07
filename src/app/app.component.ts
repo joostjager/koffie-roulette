f0498982442d5b4d6260f581af747439a7a08981
Joost Jager <joost.jager@gmail.com>
2024-07-07 12:06:34 +0200

    running counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoffeeCupDisplayComponent } from './coffee-cup-display/coffee-cup-display.component';
-import { NgIf } from '@angular/common';
+import { NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CoffeeCupDisplayComponent, NgIf],
+  imports: [RouterOutlet, CoffeeCupDisplayComponent, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,6 +21,9 @@ export class AppComponent {
 
   capuEndChance: number = 5;
   randomNumber: number = 0;
+  isRandomNumberFinal: boolean = false;
+  finalRandomNumber: number = 0;
+  stepsToGo: number = 0;
 
   onCapuCountChanged(newValue: number) {
     this.capuCount = newValue;
@@ -41,7 +44,32 @@ export class AppComponent {
   }
 
   random(): void {
-    this.randomNumber = Math.floor(Math.random() * 100) + 1;
+    this.finalRandomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(this.finalRandomNumber);
+
+    this.randomNumber = 1;
+    this.stepsToGo = this.finalRandomNumber - 1 + 500;
+
+    this.setTimer();
+  }
+
+  setTimer(): void {
+    var delay = 1 + 1000 / this.stepsToGo;
+
+    // set timer
+    setTimeout(() => {
+      this.randomNumber++;
+      if (this.randomNumber > 100)
+        this.randomNumber = 1;
+
+      this.stepsToGo--;
+
+      if (this.stepsToGo > 0)
+        this.setTimer()
+      else
+        this.isRandomNumberFinal = true;
+
+    }, delay);
   }
 }
 
